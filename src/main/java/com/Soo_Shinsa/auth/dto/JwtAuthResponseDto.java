9abb2393eb78e0ff59adc1dc08b9d49a16acd140
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-02-17 19:13:36 +0900

    refactor : "JwtAuthResponseDto" - builder 패턴 추가

--- src/main/java/com/Soo_Shinsa/auth/dto/JwtAuthResponseDto.java
+++ src/main/java/com/Soo_Shinsa/auth/dto/JwtAuthResponseDto.java
@@ -1,18 +1,25 @@
 package com.Soo_Shinsa.auth.dto;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PACKAGE)
-@AllArgsConstructor
+@NoArgsConstructor
 public class JwtAuthResponseDto {
 
   private String tokenAuthScheme;
   private String refreshToken;
   private Long refreshTokenExpiration;
   private String accessToken;
-  private String username;
+  private String email;
+
+  @Builder
+  public JwtAuthResponseDto(String tokenAuthScheme, String refreshToken, Long refreshTokenExpiration, String accessToken, String email) {
+    this.tokenAuthScheme = tokenAuthScheme;
+    this.refreshToken = refreshToken;
+    this.refreshTokenExpiration = refreshTokenExpiration;
+    this.accessToken = accessToken;
+    this.email = email;
+  }
 }
