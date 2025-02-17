690f18cff00cb45f8b1db141bb3d8a9ef8575225
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-02-17 11:36:23 +0900

    refactor : "카테고리 연관관계 변경"

--- src/main/java/com/Soo_Shinsa/category/model/Category.java
+++ src/main/java/com/Soo_Shinsa/category/model/Category.java
@@ -1,14 +1,10 @@
 package com.Soo_Shinsa.category.model;
 
-import com.Soo_Shinsa.brand.model.Brand;
 import com.Soo_Shinsa.constant.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
@@ -19,26 +15,12 @@ public class Category extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "brand_Id", nullable = false)
-    private Brand brand;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "parent_id")
-    private Category parent;
-
-    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Category> children = new ArrayList<>();
-
     @Column(nullable = false)
     private String name;
 
     @Builder
-    public Category(Brand brand, Category parent, String name, List<Category> children) {
-        this.brand = brand;
-        this.parent = parent;
+    public Category(String name) {
         this.name = name;
-        this.children = children;
     }
 
     public void update(String name) {
