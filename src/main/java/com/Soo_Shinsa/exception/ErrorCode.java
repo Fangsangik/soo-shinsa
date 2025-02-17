216b04e8219d6fb59936cff90374084e92241c74
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-02-17 11:39:11 +0900

    feat : "ErrorCode" - ErrorMessage 추가

--- src/main/java/com/Soo_Shinsa/exception/ErrorCode.java
+++ src/main/java/com/Soo_Shinsa/exception/ErrorCode.java
@@ -62,7 +62,10 @@ public enum ErrorCode {
     INVALID_CART_ITEM_PRICE("적합하지 않은 금액 입니다.", HttpStatus.BAD_REQUEST),
     COUPON_OUT_OF_STOCK("더이상 쿠폰이 존재하지 않습니다.", HttpStatus.BAD_REQUEST),
     NOT_FOUND_COUPON("쿠폰을 찾을 수 없습니다", HttpStatus.NOT_FOUND),
-    ONLY_BEFORE_PAYMENT("결제 전 상태만 이용할 수 있습니다.", HttpStatus.BAD_REQUEST);
+    ONLY_BEFORE_PAYMENT("결제 전 상태만 이용할 수 있습니다.", HttpStatus.BAD_REQUEST),
+    NOT_FOUND_SUB_CATEGORY("해당 카테고리를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+
+
     private final String message;
     private final HttpStatus httpStatus;
 
