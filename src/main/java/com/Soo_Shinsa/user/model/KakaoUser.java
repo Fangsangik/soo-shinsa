ca769ab72c71fe009f3cd91d5b16b84a63133daf
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-02-18 21:32:05 +0900

    feat : "KakaoUser" - 카카오 user entity

---
+++ src/main/java/com/Soo_Shinsa/user/model/KakaoUser.java
@@ -0,0 +1,32 @@
+package com.Soo_Shinsa.user.model;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class KakaoUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String nickname;
+
+    private Long kakaoId;
+
+    @OneToOne(fetch = FetchType.LAZY)// 관계의 종속 엔티티
+    @JoinColumn(name = "user_id") // User 테이블의 FK 설정
+    private User user;
+
+    @Builder
+    public KakaoUser(String email, String nickname, Long kakaoId, User user) {
+        this.email = email;
+        this.nickname = nickname;
+        this.kakaoId = kakaoId;
+        this.user = user;
+    }
+}
