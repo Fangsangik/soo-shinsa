74f08cb79a3135d76dc55464df98ad0be8fdd013
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-02-21 00:53:16 +0900

    fix : "KakaoUser 엔티티와, UserGrade라는 엔티티가 LazyLoading으로 연관관계를 맺고 있어, LeftJoinFetch로 한번에 조회"

--- src/main/java/com/Soo_Shinsa/user/repository/UserRepository.java
+++ src/main/java/com/Soo_Shinsa/user/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.Soo_Shinsa.user.repository;
 import com.Soo_Shinsa.exception.NotFoundException;
 import com.Soo_Shinsa.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -11,9 +13,15 @@ import static com.Soo_Shinsa.exception.ErrorCode.NOT_FOUND_USER;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
+    @Query("SELECT u FROM User u " +
+            "LEFT JOIN FETCH u.kakaoUser " +
+            "LEFT JOIN FETCH u.userGrade " +
+            "WHERE u.email = :email")
+    Optional<User> findUserWithKakaoByEmail(@Param("email") String email);
 
     default User findByEmailOrElseThrow(String email) {
-        return findByEmail(email).orElseThrow(() -> new NotFoundException(NOT_FOUND_USER));
+        return findUserWithKakaoByEmail(email)
+                .orElseThrow(() -> new NotFoundException(NOT_FOUND_USER));
     }
 
     default User findByIdOrElseThrow(Long userId){
