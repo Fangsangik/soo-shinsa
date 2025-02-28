39df445b6c584a51caad717890bde0643043224c
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-02-28 19:29:34 +0900

    refactor : - "주문 생성시 최종 금액 포함 되게 계산 로직 수정

--- src/main/java/com/Soo_Shinsa/cartitem/dto/ApplyCouponCartResponseDto.java
+++ src/main/java/com/Soo_Shinsa/cartitem/dto/ApplyCouponCartResponseDto.java
@@ -17,28 +17,37 @@ public class ApplyCouponCartResponseDto {
 
     private Long cartItemId;
     private Long productId;
-    private BigDecimal originalPrice;
+    private BigDecimal originalTotalPrice;
     private BigDecimal discountedPrice; // 추가
     private List<ProductOptionResponseDto> productOptions;
 
     @Builder
-    public ApplyCouponCartResponseDto(Long cartItemId, Long productId, BigDecimal originalPrice, BigDecimal discountedPrice, List<ProductOptionResponseDto> productOptions) {
+    public ApplyCouponCartResponseDto(Long cartItemId, Long productId, BigDecimal originalTotalPrice,  BigDecimal discountedPrice, List<ProductOptionResponseDto> productOptions) {
         this.cartItemId = cartItemId;
         this.productId = productId;
-        this.originalPrice = originalPrice;
+        this.originalTotalPrice = originalTotalPrice;
         this.discountedPrice = discountedPrice;
         this.productOptions = productOptions;
     }
 
     public static ApplyCouponCartResponseDto toDto(CartItem cartItem, List<ProductOption> productOptions) {
+        // 1. 원래 총 가격 계산 (상품 가격 * 옵션 총 수량)
+        int totalQuantity = productOptions.stream()
+                .mapToInt(ProductOption::getQuantity) // 옵션별 수량 가져오기
+                .sum(); // 모든 옵션의 수량 합산
+
+        BigDecimal originalTotalPrice = cartItem.getProduct().getPrice()
+                .multiply(BigDecimal.valueOf(totalQuantity));
+
         return ApplyCouponCartResponseDto.builder()
                 .cartItemId(cartItem.getId())
                 .productId(cartItem.getProduct().getId())
-                .originalPrice(cartItem.getProduct().getPrice())
-                .discountedPrice(cartItem.getDiscountedPrice()) // 할인된 금액 설정
+                .originalTotalPrice(originalTotalPrice) // ✅ 원래 총 가격 설정 (상품 가격 * 총 옵션 수량)
+                .discountedPrice(cartItem.getDiscountedPrice()) // 할인된 가격 설정
                 .productOptions(productOptions.stream()
                         .map(ProductOptionResponseDto::toDto)
                         .collect(Collectors.toList()))
                 .build();
     }
+
 }
